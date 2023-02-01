d95b9bb2fa3e277e3c06c7def4b01f0535940a0c
kjm4027 <gameliker16@naver.com>
2023-02-01 23:37:17 +0900

    EC

--- webdriver.py
+++ webdriver.py
@@ -1,26 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait as WDW
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import os
+import pyperclip
 
+# selenium -> chrome 에 http response 던져주기
 options = webdriver.ChromeOptions()
 options.add_argument("window-size=1000,1000")
 options.add_argument('no-sandbox')
-# options.add_argument('headless')
+# options.add_argument('headless') #화면 안 띄우기 ㅇㅋㅇㅋ?
 
-chrome = webdriver.Chrome('./chromedriver.exe',options=options)
-chrome.get('https://shopping.naver.com')
+chrome = webdriver.Chrome('./chromedriver.exe', options=options) # Debug모드 ,크롬
+# chrome.implicitly_wait(3) # 크롬드라이버가 멈춤
+short_wait = WDW(chrome,3)
 wait = WDW(chrome, 10)
 
+chrome.get('https://shopping.naver.com')
+login_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,'a#gnb_login_button'))) # a 태그의 id
+# login_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'a#gnb_login_button'))) # a 태그의 id
+print(login_button.text)
+login_button.click()
 
-def find(wait , CLASS_NAME):
-    return wait.until(EC.presence_of_element_located((By.CLASS_NAME, CLASS_NAME)))
-
-
-search = find(wait,"_searchInput_search_text_fSuJ6")
-search.send_keys('아이폰 케이스\n')   # \n이 Enter 역할임 중요
-# button = chrome.find_element(By.CLASS_NAME, '_searchInput_button_search_h79Dk')
-# button.click()
-time.sleep(5)
+time.sleep(3)
 chrome.close()
+
